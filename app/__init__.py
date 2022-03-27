e125b1b4eb434a9e2ad5ee62a1e3b815bd26ed54
K A Aditi Rao <aditirao19@gmail.com>
2022-03-27 15:30:08 +0530

    Cleaning code

--- app/__init__.py
+++ app/__init__.py
@@ -36,35 +36,6 @@ sheet = client.open("My Books").sheet1
 
 # Extract and print all of the values
 list_of_hashes = sheet.get_all_records()
-#print(type(list_of_hashes[0]))
-#print(len(list_of_hashes[1]))
-#col_val_num = len(sheet.row_values(1))
-#row_val_num = len(sheet.col_values(1))
-
-#for i in list_of_hashes:
-#    print(i)
-#print(type(sheet.row_values(1)),"\t",sheet.row_values(1),"\n",col_val_num, ",",col_val_num)
-
-#for i in list_of_hashes[0]:
-#    print(i, end="\t\t")
-    #print(list_of_hashes[0][i],end="\t") #print first row
-
-#print()
-
-#for i in range(0,len(list_of_hashes)):
-#    for j in list_of_hashes[i]:
-#        print(list_of_hashes[i][j], end="\t")
- #   print()
-
-#print(sheet.cell(2,1).value)
-#for i in  range(1,row_val_num):
-#    for j in  range(0,col_val_num):
-#        print(list_of_hashes[i][j],end="\t") #print second row
-#    print()
-#print("", sheet.col_values(1))
-
-#print(sheet.cell(1, 1).value)
-#print(list_of_hashes)
 
 #Initializing JWT App
 #jwt = JWTManager(app)
