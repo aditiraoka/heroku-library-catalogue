3bcf75a9c17bfd8a58f5783ce2f44080d90197ee
K A Aditi Rao <aditirao19@gmail.com>
2022-03-31 00:15:45 +0530

    Adding Homepage

--- app/index.py
+++ app/index.py
@@ -13,9 +13,21 @@ from flask import render_template
 
 @lib_app.route('/',methods=['GET', 'POST'])
 def hello_world():
-    return render_template("index.html")
+    list_of_hashes = sheet.get_all_records()
+    total=len(list_of_hashes)
+    read, tbr,reviewed = 0,0,0
+    for i in range(0,total):
+        if list_of_hashes[i]['Status'] == "Read":
+            read=read+1
+        if list_of_hashes[i]['Status'] == "TBR":
+            tbr=tbr+1
+        if list_of_hashes[i]['Blog Link'] != "NA":
+            reviewed=reviewed+1
+    print("read: ",read,"\ttbr: ",tbr)
+    
+    return render_template("home.html", read=read, reviewed=reviewed, total=total, tbr=tbr )
 
 @lib_app.route("/books", methods=['GET','POST'])
 def show_books():
     list_of_hashes = sheet.get_all_records()
-    return render_template("list.html", result=list_of_hashes, length=len(list_of_hashes))
+    return render_template("list_books.html", result=list_of_hashes, length=len(list_of_hashes))
