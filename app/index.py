d62b9610c6650461c59015dc4a08bc0a469105fc
K A Aditi Rao <aditirao19@gmail.com>
2022-04-01 00:18:00 +0530

    Adding: Datatables, urls in the homepage, and css correction

--- app/index.py
+++ app/index.py
@@ -10,6 +10,18 @@ from app import lib_app, sheet
 from app.libs import * 
 from flask import render_template
 
+def get_status_books(status):
+    list_of_hashes = sheet.get_all_records()
+    length=len(list_of_hashes)
+    result=[]
+    for i in range(0,length):
+        if status == "Read" or status == "TBR":
+            if list_of_hashes[i]['Status'] == status:
+                result.append(list_of_hashes[i])
+        elif status == "Reviewed":
+            if list_of_hashes[i]['Blog Link'] != "NA":
+                result.append(list_of_hashes[i])
+    return result
 
 @lib_app.route('/',methods=['GET', 'POST'])
 def hello_world():
@@ -27,6 +39,22 @@ def hello_world():
     return render_template("home.html", read=read, reviewed=reviewed, total=total, tbr=tbr )
 
 @lib_app.route("/books", methods=['GET','POST'])
-def show_books():
+def list_books():
     list_of_hashes = sheet.get_all_records()
-    return render_template("list_books.html", result=list_of_hashes, length=len(list_of_hashes))
+    #return render_template("list_books.html", result=list_of_hashes, length=len(list_of_hashes))
+    return render_template("page.html", result=list_of_hashes, length=len(list_of_hashes), status="All")
+
+@lib_app.route("/read", methods=['GET', 'POST'])
+def list_read():
+    read=get_status_books("Read")
+    return render_template("page.html", result=read, length=len(read), status="Read")
+
+@lib_app.route("/tbr", methods=['GET', 'POST'])
+def list_tbr():
+    tbr=get_status_books("TBR")
+    return render_template("page.html", result=tbr, length=len(tbr), status="To Be Read")
+
+@lib_app.route("/reviewed", methods=['GET', 'POST'])
+def list_reviewed():
+    review=get_status_books("Reviewed")
+    return render_template("page.html", result=review, length=len(review), status="Reviewed")
